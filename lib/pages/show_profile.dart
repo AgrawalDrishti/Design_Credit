5b5a5f478b5e1c1131b9c1cc33798338eb6fd0cd
AgrawalDrishti <agrawal.24@iitj.ac.in>
2024-01-19 23:19:39 +0530

    minor changes

---
+++ lib/pages/show_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowProfile extends StatefulWidget {
+  const ShowProfile({super.key});
+
+  @override
+  State<ShowProfile> createState() => _ShowProfileState();
+}
+
+class _ShowProfileState extends State<ShowProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
